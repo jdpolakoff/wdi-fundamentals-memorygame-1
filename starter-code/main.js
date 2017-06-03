9e871d5eabbcab2d696ce4d2761d206c1ea7001c
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-06-03 15:58:45 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,15 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var CardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User Flipped Queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+console.log("User Flipped King");
+
+var didYouWin = (cardsInPlay.length === 2) && (cardsInPlay[0]) === (cardsInPlay[1]) ? alert("You found a match!") : alert("Sorry, try again.")
+
+
+
