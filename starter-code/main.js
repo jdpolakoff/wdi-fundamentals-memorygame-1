3d00bbb9ad4c347cdf56cd0c329c1ee5a7cf8aaf
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-06-06 15:06:35 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,26 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -6,13 +28,14 @@ var checkForMatch = function () {
     console.log("You found a match!");
   } else {
     console.log("Sorry, try again.");
-
   }
 };
 
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suit)
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
@@ -21,5 +44,4 @@ var flipCard = function (cardId) {
 
 flipCard(0);
 flipCard(2);
-
 
